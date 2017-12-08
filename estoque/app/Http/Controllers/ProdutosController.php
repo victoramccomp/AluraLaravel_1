f230bcba0a9a5a818d0fb1aec55c55425772e63c
Victor Angelo Marcorin <victor.am.ccomp@gmail.com>
2017-12-08 10:55:57 -0200

    Aula 3 - Trabalhando com a View

--- estoque/app/Http/Controllers/ProdutosController.php
+++ estoque/app/Http/Controllers/ProdutosController.php
@@ -6,18 +6,8 @@ class ProdutosController extends Controller {
     
     public function lista() {
 
-        $html = '<h1>Listagem de produtos com Laravel</h1>';
-
-        $html .= '<ul>';
-
         $produtos = DB::select('SELECT * FROM produtos');
 
-        foreach ($produtos as $produto) {
-            $html .= '<li> Nome: '. $produto->nome .', Descrição: '. $produto->descricao .'</li>';
-        }
-
-        $html .= '</ul>';
-
-        return $html;
+        return view('listagemProdutos')->with('produtos', $produtos);
     }
 }
